956dce4cc428e46cc3c50890af21d4abb5a34151
Carlo Atripaldi <carloatrica@gmail.com>
2024-03-02 17:09:33 +0100

    Create ErrorPage.jsx

---
+++ compitoreact/src/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <>
+      <img
+        src="https://creare-sito-web-gratis.it/wp-content/uploads/2023/09/errore-404-pagina-non-trovata.jpg"
+        alt="errorPage"
+      />
+    </>
+  );
+};
+
+export default ErrorPage;
